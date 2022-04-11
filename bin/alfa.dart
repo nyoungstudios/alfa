1afec979aeffa8779502ad71ef99414aab0c5952
Nathaniel Young <nathanielyoung95070@gmail.com>
2022-04-11 07:36:56 +0000

    renames cli and writes an example config toml

--- bin/alfa.dart
+++ bin/alfa.dart
@@ -1,11 +1,14 @@
+// run with dart run alfa
+
 import 'package:toml/toml.dart';
 
 void main() async {
   var document = await TomlDocument.load('config.toml');
   var config = document.toMap();
+  
+  var title = config['main']['title'];
+  print('Running "$title" config');
+
   print('This is stuff: $config');
 
-  var title = config['title'];
-
-  print('$title');
 }
