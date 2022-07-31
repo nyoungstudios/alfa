7fc06fdb475d4018d8473384f3fd1ff93b954d93
Nathaniel Young <nathanielyoung95070@gmail.com>
2022-07-31 08:04:07 +0000

    adds install function for jetbrains

--- functions.sh
+++ functions.sh
@@ -194,3 +194,17 @@ install_deb_package() {
   apt-get install -y "$filepath"
   rm -f "$filepath"
 }
+
+install_jetbrains_toolbox() {
+  # installs the jetbrains toolbox app (also should be run in non root mode)
+  filepath="/tmp/jetbrains-toolbox.tar.gz"
+  output_folder="/tmp/jetbrains-toolbox-tmp"
+  version="jetbrains-toolbox-1.25.12627"
+  mkdir -p "$output_folder"
+  wget -O "$filepath" "https://download.jetbrains.com/toolbox/${version}.tar.gz"
+  tar -xvf "$filepath" -C "$output_folder"
+  pushd "${output_folder}/${version}"
+  ./jetbrains-toolbox
+  popd
+  rm -rf "$filepath" "$output_folder"
+}
