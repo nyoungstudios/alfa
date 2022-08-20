ce63fcee06050481dc22e87e8da8c3dd13e46b90
Nathaniel Young <nathanielyoung95070@gmail.com>
2022-08-20 22:52:58 +0000

    improves installer to handle linux and macos, error handling, and fixes installing brew

--- functions.sh
+++ functions.sh
@@ -7,7 +7,7 @@ command_exists() {
 
 install_brew() {
   # Installs homebrew
-  # NONINTERACTIVE=1
+  NONINTERACTIVE=1
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 }
 
--- install.sh
+++ install.sh
@@ -7,18 +7,38 @@ set -eu
 #   exit
 # fi
 
+# checks if install.sh is run from the correct directory
+if [[ ! -f "functions.sh" ]]; then
+  echo "install.sh must be run from the root directory of the alfa repo"
+  exit 1
+fi
 
+alfaCommand=""
+
+# sets the alfa command variable based on the operating system
+unameResults="$(uname -s)"
+if [[ "Linux" == *"$unameResults"* ]]; then
+  alfaCommand="alfa_linux"
+elif [[ "Darwin" == *"$unameResults"* ]]; then
+  alfaCommand="alfa_macos"
+else
+  echo "alfa cannot run on ${unameResults}. It can only run on Linux and macOS."
+  exit 1
+fi
+
+# runs the alfa command depending upon if sudo exists
 if ! command -v "sudo" > /dev/null 2>&1; then
   # command doesn't exist
-  ./alfa_linux "$@"
+  ./$alfaCommand "$@"
 else
   # command exists
-  sudo ./alfa_linux -u "$(whoami)" "$@"
+  sudo ./$alfaCommand -u "$(whoami)" "$@"
 fi
 
 hasHelp="0"
 hasRunShell="0"
 
+# checks if the help and run-zsh arguments were passed
 for argument in "$@"
 do
   if [[ "$argument" == "-h" || "$argument" == "--help" ]]; then
@@ -28,6 +48,7 @@ do
   fi
 done
 
+# runs zsh
 if [[ "$hasHelp" == "0" && "$hasRunShell" == "1" ]]; then
   /bin/bash -euc "source functions.sh; run_zsh"
 fi
