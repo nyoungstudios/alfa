5f0db81f4130679cc0564e5fa7d1d5080541e5b7
Nathaniel Young <nathanielyoung95070@gmail.com>
2022-09-04 23:04:14 +0000

    refactoring and reordering

--- functions.sh
+++ functions.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+# Contains the shell scripts to install all of your favorite tools
+# Do not name the function to install something the same as the command. For example, the function to install brew is not called "brew", but
+# rather "install_brew".
+# To access the list of "options" in the config.toml file you pass to the installer, use the "$@" variable.
+# To access the user that called the installer, do not user the environment variable "$SUDO_USER", but rather use "$ALFA_USER".
 
-command_exists() {
-  # tests if command exists
-  command -v "$@" >/dev/null 2>&1
+create_git_config() {
+  # sets up git config name and email
+  git config --global user.name "$1"
+  git config --global user.email "$2"
+  ssh-keygen -q -t rsa -C "$2" -N '' <<< ""$'\n'"y" 2>&1 >/dev/null
 }
 
 install_brew() {
@@ -25,6 +32,25 @@ brew_install_cask() {
   do
     brew install "$package" --cask
   done
+}
+
+apt_get_install() {
+  # installs apt-get packages
+  apt-get update
+  apt-get install -y "$@"
+}
+
+install_deb_package() {
+  # installs a deb package
+  filepath="/tmp/file_to_install.deb"
+  wget -O "$filepath" "$@"
+  apt-get install -y "$filepath"
+  rm -f "$filepath"
+}
+
+install_snap_package() {
+  # installs a single snap package
+  snap install "$@"
 }
 
 install_ohmyzsh() {
@@ -94,7 +120,7 @@ add_ohmyzsh_plugins_linux() {
   sed -i "s+### NEW PLUGINS HERE ###+${plugins}+g" ~/.zshrc
 }
 
-prettify_terminal() {
+prettify_terminal_macos() {
   # clone Powerlevel10k repo and copy .p10k.zsh config file
   echo "Cloning Powerlevel10k theme"
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
@@ -142,25 +168,34 @@ install_vimrc() {
   git config --global core.editor "vim"
 }
 
-create_git_config() {
-  # sets up git config name and email
-  git config --global user.name "$1"
-  git config --global user.email "$2"
-  ssh-keygen -q -t rsa -C "$2" -N '' <<< ""$'\n'"y" 2>&1 >/dev/null
+install_jetbrains_toolbox() {
+  # installs the jetbrains toolbox app (also should be run in non root mode)
+  filepath="/tmp/jetbrains-toolbox.tar.gz"
+  output_folder="/tmp/jetbrains-toolbox-tmp"
+  version="jetbrains-toolbox-1.25.12627"
+  mkdir -p "$output_folder"
+  wget -O "$filepath" "https://download.jetbrains.com/toolbox/${version}.tar.gz"
+  tar -xvf "$filepath" -C "$output_folder"
+  pushd "${output_folder}/${version}"
+  ./jetbrains-toolbox
+  popd
+  rm -rf "$filepath" "$output_folder"
 }
 
-git_clone_repo() {
-  # clones any number of git repositories to a local folder while retaining the
-  # repo's project name.
-  # the first argument is the local directory and all arguments following
-  # are the urls
-  dir="$1"
-  shift
-  mkdir -p "$dir"
-  for repo in "$@"
-  do
-    git -C "$dir" clone "$repo"
-  done
+install_docker() {
+  # installs docker on linux
+  apt-get update
+  apt-get install -y --no-install-recommends ca-certificates curl gnupg lsb-release
+  mkdir -p /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+  apt-get update
+  apt-get install -y --no-install-recommends docker-ce docker-ce-cli containerd.io
+
+  echo "Configuring docker to be run without calling sudo"
+  groupadd -f docker
+  gpasswd -a $ALFA_USER docker
+  echo "Please restart your computer to use docker without calling sudo"
 }
 
 install_anaconda3_common() {
@@ -203,6 +238,13 @@ install_nvm() {
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 }
 
+install_node_lts() {
+  # installs the latest node lts version
+  NVM_DIR="${NVM_DIR:-$HOME/.nvm}"
+  . $NVM_DIR/nvm.sh
+  nvm install --lts
+}
+
 install_node() {
   # installs specfied version of node; otherwise, will install latest version
   NVM_DIR="${NVM_DIR:-$HOME/.nvm}"
@@ -213,29 +255,6 @@ install_node() {
   else
     nvm install "$1"
   fi
-}
-
-install_node_lts() {
-  # installs the latest node lts version
-  NVM_DIR="${NVM_DIR:-$HOME/.nvm}"
-  . $NVM_DIR/nvm.sh
-  nvm install --lts
-}
-
-install_atom_packages() {
-  # installs atom packages
-  for package in "$@"
-  do
-    apm install "$package"
-  done
-}
-
-install_vscode_extensions() {
-  # installs vs code extensions (needs to be installed in non sudo mode)
-  for extension in "$@"
-  do
-    code --install-extension "$extension"
-  done
 }
 
 install_sdkman() {
@@ -258,51 +277,32 @@ install_sdk_candidate() {
   set -eu
 }
 
-apt_get_install() {
-  # installs apt-get packages
-  apt-get update
-  apt-get install -y "$@"
+install_atom_packages() {
+  # installs atom packages
+  for package in "$@"
+  do
+    apm install "$package"
+  done
 }
 
-install_deb_package() {
-  # installs a deb package
-  filepath="/tmp/file_to_install.deb"
-  wget -O "$filepath" "$@"
-  apt-get install -y "$filepath"
-  rm -f "$filepath"
+install_vscode_extensions() {
+  # installs vs code extensions (needs to be installed in non sudo mode)
+  for extension in "$@"
+  do
+    code --install-extension "$extension"
+  done
 }
 
-install_jetbrains_toolbox() {
-  # installs the jetbrains toolbox app (also should be run in non root mode)
-  filepath="/tmp/jetbrains-toolbox.tar.gz"
-  output_folder="/tmp/jetbrains-toolbox-tmp"
-  version="jetbrains-toolbox-1.25.12627"
-  mkdir -p "$output_folder"
-  wget -O "$filepath" "https://download.jetbrains.com/toolbox/${version}.tar.gz"
-  tar -xvf "$filepath" -C "$output_folder"
-  pushd "${output_folder}/${version}"
-  ./jetbrains-toolbox
-  popd
-  rm -rf "$filepath" "$output_folder"
-}
-
-install_snap_package() {
-  # installs a single snap package
-  snap install "$@"
-}
-
-install_docker() {
-  # installs docker on linux
-  apt-get update
-  apt-get install -y --no-install-recommends ca-certificates curl gnupg lsb-release
-  mkdir -p /etc/apt/keyrings
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-  apt-get update
-  apt-get install -y --no-install-recommends docker-ce docker-ce-cli containerd.io
-
-  echo "Configuring docker to be run without calling sudo"
-  groupadd -f docker
-  gpasswd -a $ALFA_USER docker
-  echo "Please restart your computer to use docker without calling sudo"
+git_clone_repo() {
+  # clones any number of git repositories to a local folder while retaining the
+  # repo's project name.
+  # the first argument is the local directory and all arguments following
+  # are the urls
+  dir="$1"
+  shift
+  mkdir -p "$dir"
+  for repo in "$@"
+  do
+    git -C "$dir" clone "$repo"
+  done
 }
