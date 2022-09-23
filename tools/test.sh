7bd7afc999292410beeee9e89dd377aa33e97faa
Nathaniel Young <nathanielyoung95070@gmail.com>
2022-09-23 05:01:37 +0000

    refactor: moves scripts to tools folder

--- tools/test.sh
+++ tools/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Usage: ./tests/test.sh function_name fn_arg1 fn_arg2 fn_argN
+# Usage: ./tools/test.sh function_name fn_arg1 fn_arg2 fn_argN
 
 # tests a single function
 /bin/bash -euc "source functions.sh; $*"
