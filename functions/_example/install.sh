b680acf34e48869941c38afc164b7232f084f8d3
Nathaniel Young <nathanielyoung95070@gmail.com>
2022-09-23 08:36:55 +0000

    spelling

--- functions/_example/install.sh
+++ functions/_example/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # A bash script to install something
-# Do not name the function to install something the same an existing command. For example, the function to install brew
+# Do not name the function to install something the same as an existing command. For example, the function to install brew
 # is not called "brew", but rather "install_brew".
 # To access the list of "options" in the config.toml file you pass to the installer, use the "$@" variable.
 # To access the user that called the installer, do not user the environment variable "$SUDO_USER", but rather use "$ALFA_USER".
