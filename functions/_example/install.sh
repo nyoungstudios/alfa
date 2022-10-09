b58955311648c0255b66b6716cdaa945a8fc68f4
Nathaniel Young <nathanielyoung95070@gmail.com>
2022-10-09 02:15:45 +0000

    updates documentation in example function

--- functions/_example/install.sh
+++ functions/_example/install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # A bash script to install something
-# Do not name the function to install something the same as an existing command. For example, the function to install brew
-# is not called "brew", but rather "install_brew".
-# To access the list of "options" in the config.toml file you pass to the installer, use the "$@" variable.
-# To access the user that called the installer, do not user the environment variable "$SUDO_USER", but rather use "$ALFA_USER".
-# To access the uname -m output (system architecture), you can use the environment variable "$ALFA_ARCH"
+# 1. Do not name the function to install something the same as an existing command. For example, the function to install brew
+#    is not called "brew", but rather "install_brew".
+# 2. To access the list of "options" in the config.toml file you pass to the installer, use the "$@" variable.
+# 3. To access the user that called the installer, do not user the environment variable "$SUDO_USER", but rather use "$ALFA_USER".
+# 4. To access the uname -m output (system architecture), you can use the environment variable "$ALFA_ARCH"
+# 5. To download a file from the web, you can use the curl_or_wget function (tools/download.sh) which will use curl or wget depending upon
+#    what the user has installed.
 
 install_example() {
   # example function
