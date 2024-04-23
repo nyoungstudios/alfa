342c8812b364fc01aaaff983639e875537d9cd1e
Nathaniel Young <nathanielyoung95070@gmail.com>
2024-04-23 03:53:45 +0000

    chore(functions): bumps anaconda3 release to 2024.02

--- functions/anaconda/install.sh
+++ functions/anaconda/install.sh
@@ -19,13 +19,13 @@ install_anaconda3_common() {
 install_anaconda3_macos() {
   # installs anaconda3
   echo "Installing Anaconda3..."
-  curl_or_wget "${1:-https://repo.anaconda.com/archive/Anaconda3-2022.05-MacOSX-${ALFA_ARCH}.sh}" ~/anaconda3.sh
+  curl_or_wget "${1:-https://repo.anaconda.com/archive/Anaconda3-2024.02-1-MacOSX-${ALFA_ARCH}.sh}" ~/anaconda3.sh
   install_anaconda3_common
 }
 
 install_anaconda3_linux() {
   # installs anaconda3
   echo "Installing Anaconda3..."
-  curl_or_wget "${1:-https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-${ALFA_ARCH}.sh}" ~/anaconda3.sh
+  curl_or_wget "${1:-https://repo.anaconda.com/archive/Anaconda3-2024.02-1-Linux-${ALFA_ARCH}.sh}" ~/anaconda3.sh
   install_anaconda3_common
 }
