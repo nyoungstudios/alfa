fd15e204f89ec6f8acb5c3a2c5f8016147e41609
Nathaniel Young <nathanielyoung95070@gmail.com>
2024-05-29 18:25:08 -0700

    adds awscli2 install function

---
+++ functions/awscli/README.md
@@ -0,0 +1,18 @@
+# AWS CLI
+
+Configuration for a `awscli` entry.
+
+## Description
+
+Installs [AWS CLI version 2](https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html).
+
+## Options
+
+Accepts exactly 0 arguments.
+
+## Example
+
+```toml
+[ awscli ]
+options = []
+```
---
+++ functions/awscli/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+install_awscli_macos() {
+  TMP_DIR="$(mktemp -d)"
+  curl_or_wget -s "https://awscli.amazonaws.com/AWSCLIV2.pkg" "$TMP_DIR/awscliv2.zip"
+  installer -pkg "$TMP_DIR/awscliv2.zip" -target /
+  rm -rf "$TMP_DIR"
+}
+
+install_awscli_linux() {
+  TMP_DIR="$(mktemp -d)"
+  curl_or_wget -s "https://awscli.amazonaws.com/awscli-exe-linux-$ALFA_ARCH.zip" "$TMP_DIR/awscliv2.zip"
+  unzip "$TMP_DIR/awscliv2.zip" -d "$TMP_DIR"
+  "$TMP_DIR/aws/install"
+  rm -rf "$TMP_DIR"
+}
