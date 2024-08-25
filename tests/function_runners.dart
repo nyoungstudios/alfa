0876b562e43b4b81c311511dc80bbf805307892b
Nathaniel Young <nathanielyoung95070@gmail.com>
2024-08-24 22:56:50 -0700

    try phusion baseimage

--- tests/function_runners.dart
+++ tests/function_runners.dart
@@ -1,6 +1,10 @@
 final List<Map> runners = [
-  {'os': 'ubuntu-22.04', 'image': 'ubuntu:22.04'},
-  {'os': 'ubuntu-22.04', 'image': 'ubuntu:22.04', 'platform': 'linux/arm64'},
+  {'os': 'ubuntu-22.04', 'image': 'phusion/baseimage:jammy-1.0.4'},
+  {
+    'os': 'ubuntu-22.04',
+    'image': 'phusion/baseimage:jammy-1.0.4',
+    'platform': 'linux/arm64'
+  },
   {'os': 'macos-14'},
   {'os': 'macos-13'},
 ];
