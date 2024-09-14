635b126fc7995ac01057ff3b59dded575da82c17
Nathaniel Young <nathanielyoung95070@gmail.com>
2024-09-14 15:47:05 -0700

    try this

--- functions/ohmyzsh/install.sh
+++ functions/ohmyzsh/install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 install_ohmyzsh() {
+  # eval command to support evaluating environment variable strings passed from the config
+  export ZSH="$(eval echo ${ZSH:-})"
   # install ohmyzsh
   sh -c "$(curl_or_wget -s https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 }
