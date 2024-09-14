f92fabeac62e73323d7e41c69cb73c736dfb06fb
Nathaniel Young <nyoungstudios@users.noreply.github.com>
2024-09-14 14:12:28 -0700

    feat(functions): adds awscli (#20)

---
+++ functions/awscli/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -x
+
+install_awscli_macos() {
+  TMP_DIR="$(mktemp -d)"
+  curl_or_wget -s "https://awscli.amazonaws.com/AWSCLIV2.pkg" "$TMP_DIR/awscliv2.pkg"
+  installer -pkg "$TMP_DIR/awscliv2.pkg" -target /
+  rm -rf "$TMP_DIR"
+}
+
+install_awscli_linux() {
+  TMP_DIR="$(mktemp -d)"
+  curl_or_wget -s "https://awscli.amazonaws.com/awscli-exe-linux-$ALFA_ARCH.zip" "$TMP_DIR/awscliv2.zip"
+  unzip -q "$TMP_DIR/awscliv2.zip" -d "$TMP_DIR"
+  "$TMP_DIR/aws/install"
+  rm -rf "$TMP_DIR"
+}
