51c4db1c90054d60402611891b276232d613f8db
Nathaniel Young <nyoungstudios@users.noreply.github.com>
2024-10-14 16:35:27 -0700

    test(functions): add case for nvm and bumps version (#28)

--- functions/nvm/install.sh
+++ functions/nvm/install.sh
@@ -2,9 +2,9 @@
 
 install_nvm() {
   # installs nvm
-  if [[ "$1" -eq 0 ]]; then
+  if [[ "${1:-}" -eq 0 ]]; then
     # nvm installer will not change your profile or rc file.
     export PROFILE="/dev/null"
   fi
-  curl_or_wget https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+  curl_or_wget https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 }
