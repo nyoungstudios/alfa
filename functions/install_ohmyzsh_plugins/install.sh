ce24471b15555b02054b3e5793328d4c51ffdf7f
Nathaniel Young <nathanielyoung95070@gmail.com>
2024-10-30 04:04:52 +0000

    refactor(functions): uses ZSH environment variable in install_ohmyzsh_plugins

--- functions/install_ohmyzsh_plugins/install.sh
+++ functions/install_ohmyzsh_plugins/install.sh
@@ -4,6 +4,6 @@ install_ohmyzsh_plugins() {
   # installs plugins for ohmyzsh
   for repo in "$@"
   do
-    git -C ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins clone --depth=1 "$repo"
+    git -C "${ZSH_CUSTOM:-${ZSH:-$HOME/.oh-my-zsh}/custom}/plugins" clone --depth=1 "$repo"
   done
 }
