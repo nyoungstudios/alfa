57538b2f26b44791a4c8297977115a757f3db97e
Nathaniel Young <nyoungstudios@users.noreply.github.com>
2026-05-25 09:58:35 +0000

    supports sh for git config

--- functions/git_config/install.sh
+++ functions/git_config/install.sh
@@ -4,5 +4,6 @@ create_git_config() {
   # sets up git config name and email
   git config --global user.name "$1"
   git config --global user.email "$2"
-  ssh-keygen -q -t rsa -C "$2" -N '' <<< ""$'\n'"y" 2>&1 >/dev/null
+  # Send default file path (blank line) and confirm overwrite with "y".
+  printf '\ny\n' | ssh-keygen -q -t rsa -C "$2" -N '' >/dev/null 2>&1
 }
