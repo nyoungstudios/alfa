8c0d6c0c4b6d0412cd6ef61223f6d1e1a3ab2684
Nathaniel Young <nyoungstudios@users.noreply.github.com>
2026-05-25 01:12:44 +0000

    bumps version and adds support for arm

--- functions/jetbrains_toolbox/install.sh
+++ functions/jetbrains_toolbox/install.sh
@@ -4,17 +4,15 @@ install_jetbrains_toolbox() {
   # installs the jetbrains toolbox app (also should be run in non root mode)
   filepath="/tmp/jetbrains-toolbox.tar.gz"
   output_folder="/tmp/jetbrains-toolbox-tmp"
-  version="jetbrains-toolbox-1.25.12627"
+  version="jetbrains-toolbox-3.4.3.81140"
+  if [[ "$ALFA_ARCH" == "aarch64" ]]; then
+    version+="-arm64"
+  fi
   mkdir -p "$output_folder"
   curl_or_wget "https://download.jetbrains.com/toolbox/${version}.tar.gz" "$filepath"
   tar -xvf "$filepath" -C "$output_folder"
   pushd "${output_folder}/${version}"
-  if ldconfig -p 2>/dev/null | grep -q "libfuse.so.2"; then
-    ./jetbrains-toolbox
-  else
-    ./jetbrains-toolbox --appimage-extract >/dev/null
-    APPDIR="$(pwd)/squashfs-root" ./squashfs-root/AppRun
-  fi
+  ./bin/jetbrains-toolbox
   popd
   rm -rf "$filepath" "$output_folder"
 }
