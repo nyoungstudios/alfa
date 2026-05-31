f4c6d5ccefb95bdc3e6b94ec52dfc2089ea2518b
Nathaniel Young <nathanielyoung95070@gmail.com>
2026-05-30 21:33:16 -0700

    sh compatibile

--- functions/git_clone_and_run_script/install.sh
+++ functions/git_clone_and_run_script/install.sh
@@ -17,18 +17,20 @@ install_git_clone_and_run_script() {
   shift
   git -C "$dir" clone "$repo"
   repoDir="$(basename "$repo" .git)"
-  pushd "$repoDir" > /dev/null
 
-  # executes commands within the repo's directory
-  for cmd in "$@"
-  do
-    sh -c "$cmd"
-  done
+  (
+    cd "$repoDir"
+
+    # executes commands within the repo's directory
+    for cmd in "$@"
+    do
+      sh -c "$cmd"
+    done
+  )
 
   # removes repo's directory if the env var is set to 1 or true
   if [ "${ALFA_REMOVE_REPO:-}" == "1" ] || [ "$(echo "${ALFA_REMOVE_REPO:-}" | tr '[:upper:]' '[:lower:]')" == "true" ];
   then
-    popd > /dev/null
     rm -rf "$repoDir"
   fi
 }
